92a86bb9879262fea97f6a8c322d23c674dd0c83
Geoff Kizer <geoffrek@microsoft.com>
2017-03-10 13:01:42 -0800

    httpserver stuff

---
+++ httpserver/plaintext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Managed;
+using System.Threading.Tasks;
+
+class Test
+{
+    public static void Main()
+    {
+        HttpServer server = new HttpServer(new IPEndPoint(IPAddress.Loopback, 5000), new PlaintextHandler());
+        server.Run();
+
+        Console.WriteLine("Running");
+        Console.ReadLine();
+    }
+}
