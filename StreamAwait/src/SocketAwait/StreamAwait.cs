4e18c1e667caf2b72211cd60092a46b83c2ebc6b
Geoff Kizer <geoffrek>
2017-06-23 23:09:59 -0700

    initial cut

---
+++ StreamAwait/src/SocketAwait/StreamAwait.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketAwait
+{
+    public class StreamAwait
+    {
+        public static Socket s_listenSocket;
+
+        public const bool s_trace = false;
+        public const bool s_useSsl = false;
+
+        public static readonly byte[] s_responseMessage = Encoding.UTF8.GetBytes(
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n");
+
+//        public const int s_expectedReadSize = 848;
+
+        sealed class Connection
+        {
+            private const int BufferSize = 4096;
+
+            private Stream _stream;
+            private byte[] _readBuffer = new byte[BufferSize];
+
+            public Connection(Stream stream)
+            {
+                _stream = stream;
+            }
+
+            public async void Run()
+            {
+                if (s_trace)
+                {
+                    Console.WriteLine("Connection accepted");
+                }
+
+                while (true)
+                {
+                    int bytesRead;
+                    try
+                    {
+                        bytesRead = await _stream.ReadAsync(_readBuffer, 0, BufferSize);
+                    }
+                    catch (SocketException e)
+                    {
+                        if (e.SocketErrorCode == SocketError.ConnectionReset)
+                        {
+                            _stream.Dispose();
+                            return;
+                        }
+
+                        throw;
+                    }
+
+                    if (bytesRead == 0)
+                    {
+                        if (s_trace)
+                        {
+                            Console.WriteLine("Connection closed by client");
+                        }
+
+                        _stream.Dispose();
+                        break;
+                    }
+
+                    if (s_trace)
+                    {
+                        Console.WriteLine("Read complete, bytesRead = {0}", bytesRead);
+                    }
+
+                    await _stream.WriteAsync(s_responseMessage, 0, s_responseMessage.Length);
+
+                    if (s_trace)
+                    {
+                        Console.WriteLine("Write complete");
+                    }
+                }
+            }
+        }
+
+        private static void HandleConnection(Socket s)
+        {
+            s.NoDelay = true;
+            Stream stream = s.GetStream();
+
+            // TODO: SSL
+
+            var c = new Connection(stream);
+            c.Run();
+        }
+
+        private static void AcceptConnections()
+        {
+            while (true)
+            {
+                Socket s = s_listenSocket.Accept();
+                Task.Run(() => HandleConnection(s));
+            }
+        }
+
+        private static void Start()
+        {
+            s_listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s_listenSocket.Bind(new IPEndPoint(IPAddress.Any, 5000));
+            s_listenSocket.Listen(1000);
+
+            Task.Run(() => AcceptConnections());
+        }
+
+        public static void Main(string[] args)
+        {
+            Start();
+
+            Console.WriteLine("Server Running");
+            Console.ReadLine();
+        }
+    }
+}
+
