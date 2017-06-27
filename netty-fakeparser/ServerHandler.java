6db1fc364d6cb2c3e5e087cc86bba678122853dc
Geoff Kizer <geoffrek>
2017-06-26 21:15:28 -0700

    cloned from old code

---
+++ netty-fakeparser/ServerHandler.java
@@ -0,0 +1,74 @@
+//package io.netty.example.discard;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import static java.nio.charset.StandardCharsets.*;
+import static io.netty.buffer.Unpooled.*;
+
+/**
+ * Handles a server-side channel.
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    static final int s_expectedReadSize = 848;
+    static final byte[] s_responseMessage = (
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n")
+            .getBytes(UTF_8);
+
+    int _bytesRead = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // (Try to) validate message length
+        ByteBuf buf = (ByteBuf)msg;
+        
+        System.out.println("Read bytes: " + buf.readableBytes());
+
+        _bytesRead += buf.readableBytes();
+
+        // Release the message
+        buf.release();
+
+        if (_bytesRead < s_expectedReadSize)
+        {
+            System.out.println("_bytesRead was less than expected: " + _bytesRead);
+            return;
+        }
+
+        if (_bytesRead > s_expectedReadSize)
+        {
+            System.out.println("holy shit, _bytesRead = " + _bytesRead);
+        }
+
+        _bytesRead = 0;
+
+        // Write the response
+        ctx.write(wrappedBuffer(s_responseMessage)); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ netty-fakeparser/run.bat
@@ -0,0 +1,1 @@
+java -cp ".;netty-all-4.1.7.Final.jar" Server
