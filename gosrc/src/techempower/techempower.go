55950e1a6e77f0d61a91754205f7d061bcfa5374
Geoff Kizer <geoffrek>
2017-07-07 21:22:00 -0700

    add SSL impl

--- gosrc/src/techempower/techempower.go
+++ gosrc/src/techempower/techempower.go
@@ -2,26 +2,40 @@ package main
 
 import "log"
 import "net"
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"crypto/rsa"
+	"crypto/rand"
+	"crypto/tls"
+	"math/big"
+	"time"
+	"fmt"
+	"encoding/pem"
+	"os"
+	"bufio"
+)
 
 const s_trace = false
-const s_expectedReadSize = 848
+const s_expectedReadSize = 2624
+
 var s_responseMessage = []byte(
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
-	"HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n")
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+        "HTTP/1.1 200 OK\r\nServer: TestServer\r\nDate: Sun, 06 Nov 1994 08:49:37 GMT\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n")
 
 func handle_connection(c net.Conn) {
     buf := make([]byte, 4096)
@@ -67,16 +81,73 @@ func handle_connection(c net.Conn) {
 	}
 }
 
-func main() {
-	// Listen on TCP port 5000 on all interfaces.
-	listener, err := net.Listen("tcp", ":5000")
-	if err != nil {
-		log.Fatal(err)
+func create_cert() (tlsCert tls.Certificate, err error) {
+	// Code from: https://www.socketloop.com/tutorials/golang-create-x509-certificate-private-and-public-keys
+	// Also: https://ericchiang.github.io/post/go-tls/
+
+	// ok, lets populate the certificate with some data
+	// not all fields in Certificate will be populated
+	// see Certificate structure at
+	// http://golang.org/pkg/crypto/x509/#Certificate
+	template := &x509.Certificate {
+			IsCA : true,
+			BasicConstraintsValid : true,
+			SubjectKeyId : []byte{1,2,3},
+			SerialNumber : big.NewInt(1234),
+			Subject : pkix.Name{
+					Country : []string{"Earth"},
+					Organization: []string{"Mother Nature"},
+			},
+			NotBefore : time.Now(),
+			NotAfter : time.Now().AddDate(5,5,5),
+			// see http://golang.org/pkg/crypto/x509/#KeyUsage
+			ExtKeyUsage : []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+			KeyUsage : x509.KeyUsageDigitalSignature|x509.KeyUsageCertSign,
 	}
-	defer listener.Close()
-    
-    log.Printf("Server running")
-    
+
+	// generate private key
+	privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	publickey := &privatekey.PublicKey
+
+	// create a self-signed certificate. template = parent
+	var parent = template
+	certDER, err := x509.CreateCertificate(rand.Reader, template, parent, publickey, privatekey)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// cert, err := x509.ParseCertificate(certDER)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+    b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+    certPEM := pem.EncodeToMemory(&b)
+
+	// PEM encode the private key
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privatekey),
+	})
+	
+	tlsCert, err = tls.X509KeyPair(certPEM, privateKeyPEM)
+	if err != nil {
+		log.Fatalf("invalid key pair: %v", err)
+	}
+
+	return 
+}
+
+func accept_connections(listener net.Listener) {
     for {
 		// Wait for a connection.
 		conn, err := listener.Accept()
@@ -90,4 +161,32 @@ func main() {
         
         go handle_connection(conn)
     }
+}
+
+func main() {
+	// Listen on TCP port 5000 on all interfaces.
+	listener, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    go accept_connections(listener)
+
+    cert, err := create_cert()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+	sslListener, err := tls.Listen("tcp", ":5001", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+    go accept_connections(sslListener)
+
+    log.Printf("Server running")
+
+	reader := bufio.NewReader(os.Stdin)
+	_, _ = reader.ReadString('\n')	
 }
