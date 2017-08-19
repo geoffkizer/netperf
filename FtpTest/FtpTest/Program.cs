8c5d9b12847cec85bf8f04ef9c917744b4a2461f
Geoff Kizer <geoffrek>
2017-08-19 09:46:20 -0700

    add ClientHandler

---
+++ FtpTest/FtpTest/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace FtpTest
+{
+    class Program
+    {
+        private const string absoluteUri = "ftp://localhost/";
+
+
+        static void Bug1()
+        {
+            string resource = absoluteUri + "LargeFile";
+            FtpWebRequest r1 = (FtpWebRequest)WebRequest.Create(resource);
+            r1.Method = WebRequestMethods.Ftp.DownloadFile;
+            FtpWebResponse resp1 = (FtpWebResponse)r1.GetResponse();
+            resp1.Close();
+        }
+
+        static void PrintException(string name, Action a)
+        {
+            try
+            {
+                a();
+                Console.WriteLine("{0}: No exception", name);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}: Exception: {1}", name, e);
+            }
+        }
+
+        static void DnsError()
+        {
+            FtpWebRequest r1 = (FtpWebRequest)WebRequest.Create("ftp://nosuchhost.nosuchhost");
+            r1.Method = WebRequestMethods.Ftp.DownloadFile;
+            FtpWebResponse resp1 = (FtpWebResponse)r1.GetResponse();
+            resp1.Close();
+        }
+
+        static void ConnectError()
+        {
+            FtpWebRequest r1 = (FtpWebRequest)WebRequest.Create("ftp://192.168.123.123");
+            r1.Method = WebRequestMethods.Ftp.DownloadFile;
+            FtpWebResponse resp1 = (FtpWebResponse)r1.GetResponse();
+            resp1.Close();
+        }
+
+        static void RawDnsError()
+        {
+            TcpClient t = new TcpClient();
+            t.Connect("nosuchhost.nosuchhost", 21);
+        }
+
+        static void RawConnectError()
+        {
+            TcpClient t = new TcpClient();
+            t.Connect(IPAddress.Parse("192.168.123.123"), 21);
+        }
+
+        static void Stuff()
+        {
+            FtpWebRequest r = (FtpWebRequest)WebRequest.Create("ftp://foo/");
+            Console.WriteLine("CachePolicy = {0}", r.CachePolicy);
+
+            FtpWebRequest r2 = (FtpWebRequest)WebRequest.Create("ftps://foo/");
+
+            PrintException("DnsError", DnsError);
+            PrintException("ConnectError", ConnectError);
+
+            PrintException("RawDnsError", RawDnsError);
+            PrintException("RawConnectError", RawConnectError);
+        }
+
+        static void AsyncNonPassive()
+        {
+            string resource = "ftp://localhost/ReadOnlyResource";
+            FtpWebRequest r1 = (FtpWebRequest)WebRequest.Create(resource);
+            r1.UsePassive = false;
+            r1.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            var mem = new MemoryStream();
+            Task t = r1.GetResponseAsync().ContinueWith(t1 =>
+            {
+                FtpWebResponse resp1 = (FtpWebResponse)t1.Result;
+
+                resp1.GetResponseStream().CopyToAsync(mem);
+                resp1.Close();
+            });
+
+            t.Wait();
+
+            if (t.IsFaulted)
+                throw t.Exception;
+        }
+
+        static void Main(string[] args)
+        {
+            AsyncNonPassive();
+        }
+    }
+}
+
---
+++ SocketNewAwait/src/SocketNewAwait/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketNewAwait
+{
+    public class Program
+    {
+        public static Socket s_listenSocket;
+
+        public const bool s_trace = false;
+
+        public static readonly byte[] s_responseMessage = Encoding.UTF8.GetBytes(
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n" +
+            "HTTP/1.1 200 OK\r\nServer: TestServer\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nhello world\r\n");
+
+        public const int s_expectedReadSize = 848;
+
+        class Connection
+        {
+            private Socket _socket;
+            private byte[] _readBuffer = new byte[4096];
+            private Func<Socket, AsyncOperationTask<Socket>> _doAccept;
+            private Func<Socket, ArraySegment<byte>, SocketFlags, AsyncOperationTask<int>> _doReceive;
+            private Func<Socket, ArraySegment<byte>, SocketFlags, AsyncOperationTask<int>> _doSend;
+
+            public Connection()
+            {
+                _doAccept = AsyncOperationHelper.MakeAcceptOperation();
+                _doReceive = AsyncOperationHelper.MakeReceiveOperation();
+                _doSend = AsyncOperationHelper.MakeSendOperation();
+            }
+
+            public async void Run()
+            {
+                _socket = await _doAccept(s_listenSocket);
+
+                if (s_trace)
+                {
+                    Console.WriteLine("Connection accepted");
+                }
+
+                // Spawn another work item to handle next connection
+                QueueConnectionHandler();
+
+                _socket.NoDelay = true;
+
+                while (true)
+                {
+                    int bytesRead;
+                    try
+                    {
+                        bytesRead = await _doReceive(_socket, new ArraySegment<byte>(_readBuffer), SocketFlags.None);
+                    }
+                    catch (SocketException e)
+                    {
+                        if (e.SocketErrorCode == SocketError.ConnectionReset)
+                        {
+                            _socket.Dispose();
+                            return;
+                        }
+
+                        throw;
+                    }
+
+                    if (bytesRead == 0)
+                    {
+                        if (s_trace)
+                        {
+                            Console.WriteLine("Connection closed by client");
+                        }
+
+                        _socket.Dispose();
+                        break;
+                    }
+
+                    if (s_trace)
+                    {
+                        Console.WriteLine("Read complete, bytesRead = {0}", bytesRead);
+                    }
+
+                    if (bytesRead != s_expectedReadSize)
+                    {
+                        throw new Exception(string.Format("unexpected read size, bytesRead = {0}", bytesRead));
+                    }
+
+                    int bytesWritten = await _doSend(_socket, new ArraySegment<byte>(s_responseMessage), SocketFlags.None);
+
+                    if (s_trace)
+                    {
+                        Console.WriteLine("Write complete, bytesWritten = {0}", bytesWritten);
+                    }
+                }
+            }
+        }
+
+        private static void HandleConnection(object state)
+        {
+            var c = new Connection();
+            c.Run();
+        }
+
+        private static void QueueConnectionHandler()
+        {
+            ThreadPool.QueueUserWorkItem(HandleConnection);
+        }
+
+        private static void Start()
+        {
+            s_listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s_listenSocket.Bind(new IPEndPoint(IPAddress.Any, 5000));
+            s_listenSocket.Listen(1000);
+
+            QueueConnectionHandler();
+        }
+
+        public static void Main(string[] args)
+        {
+            Start();
+
+            Console.WriteLine("Server Running");
+            Console.ReadLine();
+        }
+    }
+}
+
