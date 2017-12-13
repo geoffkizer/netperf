063d14ccd3e8d0f67525d6b856a506556467b455
Geoff Kizer <geoffrek>
2017-12-13 11:02:49 -0800

    add command line options

--- SocketPerfTest-Direct/Program.cs
+++ SocketPerfTest-Direct/Program.cs
@@ -1,26 +1,87 @@
 ﻿using System;
 using System.Net;
 using System.Threading;
+using CommandLine;
 
 namespace SocketPerfTest
 {
     class Program
     {
-        static void RunServer()
+        [Verb("clrthreads", HelpText = "Run server on CLR thread pool.")]
+        private class ClrThreadsOptions
+        {
+        }
+
+        [Verb("customthreads", HelpText = "Run server on custom thread pool.")]
+        private class CustomThreadsOptions
+        {
+        }
+
+        [Verb("threadbound", HelpText = "Run server on thread-bound IO threads.")]
+        private class ThreadBoundOptions
+        {
+            [Option('b', "batchSize", Default = 1, HelpText = "Batch size to GetQueuedCompletionStatusEx")]
+            public int BatchSize { get; set; }
+        }
+
+        static int RunClrThreads(ClrThreadsOptions options)
         {
             IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5000);
 
             Console.WriteLine($"Running server on {endPoint} (raw)");
+            Console.WriteLine($"Running on CLR thread pool");
             ServerListener.Run(endPoint);
 
             Console.WriteLine($"Server running");
 
             Thread.Sleep(Timeout.Infinite);
+
+            return 1;
+        }
+
+        static int RunCustomThreads(CustomThreadsOptions options)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5000);
+
+            Console.WriteLine($"Running server on {endPoint} (raw)");
+            Console.WriteLine($"Running on custom thread pool");
+//            ServerListener.Run(endPoint);
+
+            Console.WriteLine($"Server running");
+
+            Thread.Sleep(Timeout.Infinite);
+
+            return 1;
+        }
+
+        static int RunThreadBound(ThreadBoundOptions options)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5000);
+
+            Console.WriteLine($"Running server on {endPoint} (raw)");
+            Console.WriteLine($"Running on thread-bound IO threads with batch size = {options.BatchSize}");
+//            ServerListener.Run(endPoint);
+
+            Console.WriteLine($"Server running");
+
+            Thread.Sleep(Timeout.Infinite);
+
+            return 1;
         }
 
         static void Main(string[] args)
         {
-            RunServer();
+            var parser = new Parser(settings =>
+            {
+                settings.HelpWriter = Console.Out;
+            });
+
+            parser.ParseArguments<ClrThreadsOptions, CustomThreadsOptions, ThreadBoundOptions>(args).MapResult(
+                (ClrThreadsOptions opts) => RunClrThreads(opts),
+                (CustomThreadsOptions opts) => RunCustomThreads(opts),
+                (ThreadBoundOptions opts) => RunThreadBound(opts),
+                _ => 1
+            );
         }
     }
 }
