fda7129779e0804fdf1802c4365a10fcfb3bda96
Geoffrey Kizer <geoffrek@windows.microsoft.com>
2021-09-14 11:38:11 -0700

    empty project

---
+++ EventLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
