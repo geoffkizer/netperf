d25634b1b5b67dc8ef8d2c92074a79b4c1d5699b
Geoffrey Kizer <geoffrek@windows.microsoft.com>
2021-11-12 13:22:27 -0800

    add sample app and EventRegsitration

--- EventLoop/EventLoop.cs
+++ EventLoop/EventLoop.cs
@@ -4,77 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EventLoop
+namespace MyApp
 {
+    internal struct EventRegistration
+    {
+        public void Dispose() { }
+    }
+
     internal class EventLoop
     {
-        // TODO: Really need to figure out the startup model.
-
-        public static Task Start(Action action)
+        public static Task Start(Action<EventLoop> action)
         {
             return Task.CompletedTask;
         }
 
-        // TODO: Take async actions too, i.e. Func<Task>
-
-        public void On(Task t, Action action)
+        public EventRegistration On(Task t, Action action)
         {
-
+            return default;
         }
 
-        public void On(Task t, Func<Task> action)
+        public EventRegistration On(Task t, Func<Task> action)
         {
-
+            return default;
         }
 
-        public void On<T>(Task<T> t, Action<T> action)
+        public EventRegistration On<T>(Task<T> t, Action<T> action)
         {
-
+            return default;
         }
 
-        public void On<T>(Task<T> t, Func<T, Task> action)
+        public EventRegistration On<T>(Task<T> t, Func<T, Task> action)
         {
-
+            return default;
         }
 
-        public void OnEvery(Func<Task> f, Action action)
+        public EventRegistration OnEvery(Func<Task> f, Action action)
         {
-
+            return default;
         }
 
-        public void OnEvery(Func<Task> f, Func<Task> action)
+        public EventRegistration OnEvery(Func<Task> f, Func<Task> action)
         {
-
+            return default;
         }
 
-        public void OnEvery<T>(Func<Task<T>> t, Action<T> action)
+        public EventRegistration OnEvery<T>(Func<Task<T>> t, Action<T> action)
         {
-
+            return default;
         }
 
-        public void OnEvery<T>(Func<Task<T>> t, Func<T, Task> action)
+        public EventRegistration OnEvery<T>(Func<Task<T>> t, Func<T, Task> action)
         {
-
+            return default;
         }
 
-        public void After(int milliseconds, Action action)
+        public EventRegistration After(int milliseconds, Action action)
         {
-
+            return default;
         }
 
-        public void After(int milliseconds, Func<Task> action)
+        public EventRegistration After(int milliseconds, Func<Task> action)
         {
-
+            return default;
         }
 
-        public void AfterEvery(int milliseconds, Action action)
+        public EventRegistration AfterEvery(int milliseconds, Action action)
         {
-
+            return default;
         }
 
-        public void AfterEvery(int milliseconds, Func<Task> action)
+        public EventRegistration AfterEvery(int milliseconds, Func<Task> action)
         {
-
+            return default;
         }
 
         public void Run(Action action)
--- EventLoop/Program.cs
+++ EventLoop/Program.cs
@@ -1,14 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using MyApp;
 
-namespace MyApp
+public class Program
 {
-    public class Program
+    public static async Task Main()
     {
-        public static void Main(string[] args)
+        Task t = EventLoop.Start(eventLoop =>
         {
-            Console.WriteLine("Hello World!");
-        }
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Register an event handler for a single-shot event
+            eventLoop.On(clientSocket.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 1234)),
+                async () =>
+                {
+                    await clientSocket.SendAsync(new byte[] { 1, 2, 3 }, SocketFlags.None);
+
+                    byte[] buffer = new byte[1024];
+                    await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
+
+                    clientSocket.Close();
+                });
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(new IPEndPoint(IPAddress.Loopback, 1234));
+            listenSocket.Listen(1);
+
+            // Register an event handler for a repeating event
+            eventLoop.OnEvery(() => listenSocket.AcceptAsync(),
+                async serverSocket =>
+                {
+                    byte[] buffer = new byte[1024];
+                    await serverSocket.ReceiveAsync(buffer, SocketFlags.None);
+
+                    await serverSocket.SendAsync(new byte[] { 1, 2, 3 }, SocketFlags.None);
+
+                    serverSocket.Close();
+                });
+
+            // Shortcuts for timers
+            eventLoop.After(500, () => { Console.WriteLine("500 milliseconds have passed"); });
+            eventLoop.AfterEvery(5000, () => { Console.WriteLine("Invoke cleanup/scavenge logic here, every 5 seconds"); });
+
+            // Example of deregistering an event handler
+            int i = 0;
+            EventRegistration eventRegistration = eventLoop.AfterEvery(1000, () =>
+            {
+                Console.WriteLine($"Iteration {i}"); 
+                i++;
+
+                // Stop after 100 iterations
+                if (i == 100)   
+                    eventRegistration.Dispose();
+            });
+
+            // Just run an arbitrary piece of async code on the event loop.
+            // Equivalent to EventLoop.On(Task.CompletedTask, ...)
+            eventLoop.Run(async () =>
+            {
+                Console.WriteLine("Hello");
+                await Task.Delay(1000);
+                Console.WriteLine("World");
+            });
+        });
+
+        // Wait for event loop to complete
+        await t;
     }
 }
